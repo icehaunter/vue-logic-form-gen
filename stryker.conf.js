d2fb92e3d10f66ca488bcb12907d8b2c2850cb36
Ilya Borovitinov <icehaunter@gmail.com>
2019-08-23 16:10:08 +0300

    Added stryker for mutation testing

---
+++ stryker.conf.js
@@ -0,0 +1,15 @@
+// This config was generated using a preset.
+// Please see the handbook for more information: https://github.com/stryker-mutator/stryker-handbook/blob/master/stryker/guides/vuejs.md#vuejs
+module.exports = function (config) {
+  config.set({
+    // mutate: ['src/**/*.js', 'src/**/*.ts', 'src/**/*.vue' ],
+    mutate: ['src/**/*.js', 'src/**/*.ts', '!src/*.ts'],
+    mutator: 'vue',
+    testRunner: 'jest',
+    jest: {
+      config: require('./jest.config')
+    },
+    reporters: ['progress', 'clear-text', 'html'],
+    coverageAnalysis: 'off'
+  })
+}
