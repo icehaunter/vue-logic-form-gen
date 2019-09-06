956cab89b3a89360ecf04fb1f93adf94b9148412
Илья Боровитинов <ilya.borovitinov@sl-tech.ru>
2019-09-06 18:38:19 +0300

    Added value wrappers of three kinds and modifier chains with resolution

---
+++ src/components/form-generator/logic/index.ts
@@ -0,0 +1,32 @@
+import { resolveModelPath, Context } from '../schema/resolution'
+import { Value, isValueFromModel, isValueBuilder } from './value'
+import { resolveModifierChain } from './modifiers'
+
+class ValueUndefinedError extends Error {
+  path: string
+  context: Context
+
+  constructor (path: string, context: Context) {
+    super(`Value resolution failed: property is undefined; path: ${path}`)
+    this.path = path
+    this.context = context
+  }
+}
+
+export function resolveValue<T> (value: Value<T>, model: any, context: Context): T {
+  if (typeof value !== 'object' || value === null) {
+    return value
+  } else if (isValueFromModel(value)) {
+    const result = resolveModelPath(value._modelPath, model, context)
+    if (result !== undefined) {
+      return result
+    } else {
+      throw new ValueUndefinedError(value._modelPath, context)
+    }
+  } else if (isValueBuilder(value)) {
+    const initialValue = resolveValue(value._buildFrom, model, context)
+    return resolveModifierChain(initialValue, value._actions, model, context)
+  } else {
+    return value
+  }
+}
---
+++ src/components/form-generator/logic/modifiers/index.ts
@@ -0,0 +1,67 @@
+import { BasicType, ConvertAnyToBasic, ConvertBasicToAny } from '../types'
+import { BuildModifiers } from './baseTypes'
+import { BooleanModifierTypes, modifiers as booleanModifiers } from './type/boolean'
+import { StringModifierTypes, modifiers as stringModifiers } from './type/string'
+import { DateModifierTypes, modifiers as dateModifiers } from './type/date'
+import { NumberModifierTypes, modifiers as numberModifiers } from './type/number'
+import { ArrayModifierTypes, modifiers as arrayModifiers } from './type/array'
+import { ObjectModifierTypes, modifiers as objectModifiers } from './type/object'
+import { Context } from '../../schema/resolution'
+import { resolveValue } from '..'
+import { Value, UnwrapValueArray } from '../value'
+
+type BooleanModifiers = BuildModifiers<BooleanModifierTypes>
+type StringModifiers = BuildModifiers<StringModifierTypes>
+type DateModifiers = BuildModifiers<DateModifierTypes>
+type NumberModifiers = BuildModifiers<NumberModifierTypes>
+type ArrayModifiers = BuildModifiers<ArrayModifierTypes>
+type ObjectModifiers = BuildModifiers<ObjectModifierTypes>
+
+type Modifiers = {
+  'boolean': BooleanModifiers,
+  'string': StringModifiers,
+  'date': DateModifiers,
+  'number': NumberModifiers
+  'array': ArrayModifiers
+  'object': ObjectModifiers
+}
+
+const modifiers = {
+  'boolean': booleanModifiers,
+  'string': stringModifiers,
+  'date': dateModifiers,
+  'number': numberModifiers,
+  'array': arrayModifiers,
+  'object': objectModifiers
+}
+
+export type Modifier = BooleanModifiers | StringModifiers | DateModifiers | NumberModifiers | ArrayModifiers | ObjectModifiers
+
+export type ModifierChain<T extends BasicType> = [Modifiers[T], ...Array<Modifier>]
+
+function convertValues<K extends Array<any>> (values: K, model: any, context: Context): UnwrapValueArray<K> {
+  return values.map(<T>(v: Value<T>): T => resolveValue(v, model, context)) as UnwrapValueArray<K>
+}
+
+const modifierReduce = (model: any, context: Context) => <T extends Modifier> (
+  { value, type }: { value: ConvertBasicToAny<T[0]>, type: BasicType | undefined },
+  [ newType, command, args, nextType ]: T,
+  index: number
+) => {
+  if (type !== undefined && type !== newType) {
+    throw new TypeError(`Modifier chain is invalid: expected command for type "${type}", recieved for type "${newType}" at action #${index}`)
+  }
+
+  const resolvedArgs = convertValues(args, model, context)
+  const newValue = (modifiers[newType] as any)[command](value, ...resolvedArgs)
+
+  return {
+    value: newValue,
+    type: nextType
+  }
+}
+
+export function resolveModifierChain<T> (initialValue: T, chain: ModifierChain<ConvertAnyToBasic<T>>, model: any, context: Context): any {
+  const reducer = modifierReduce(model, context)
+  return chain.reduce(reducer, { value: initialValue as any, type: undefined as BasicType | undefined })['value']
+}
