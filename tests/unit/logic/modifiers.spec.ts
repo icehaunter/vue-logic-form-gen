20487628fc8328717d2e8c64487ef65cc4a3a270
Илья Боровитинов <ilya.borovitinov@sl-tech.ru>
2019-09-06 18:39:16 +0300

    Added basic tests for modifier chain resolution

--- tests/unit/logic/modifiers.spec.ts
+++ tests/unit/logic/modifiers.spec.ts
@@ -1,25 +1,35 @@
-import { ModifierChain } from '@/components/form-generator/logic/modifiers'
+import {
+  ModifierChain,
+  resolveModifierChain
+} from '@/components/form-generator/logic/modifiers'
 
-describe('Value modification', () => {
-  it('Should modify a specified value', () => {
-    const chain: ModifierChain = [
-      ['add', 5],
-      ['sub', 2],
-      ['double'],
-      [
-        ['div', { modelPath: 'otherValue' }],
-        ['sub', 4]
-      ]
+describe('Modifier chain resolution', () => {
+  it('should resolve a basic modifier chain from one link', () => {
+    const chain: ModifierChain<'string'> = [
+      ['string', 'join', [' appended'], 'string']
     ]
 
-    const level = {
-      type: 'if',
-      level: 'test',
-      modelPath: 'value',
-      predicate: {
-        type: 'number',
-        lhsMod: chain
-      }
-    }
+    const result = resolveModifierChain('initial', chain, {}, [])
+
+    expect(result).toBe('initial appended')
+  })
+
+  it('should resolve a basic modifier chain from multiple links', () => {
+    const chain: ModifierChain<'string'> = [
+      ['string', 'join', ['10 symbols'], 'string'],
+      ['string', 'length', [], 'number']
+    ]
+
+    const result = resolveModifierChain('10 symbols', chain, {}, []) as number
+
+    expect(result).toBe(20)
+  })
+
+  it('should resolve throw an error if modifier chain is invalid', () => {
+    const chain: ModifierChain<'string'> = [
+      ['string', 'join', ['10 symbols'], 'string'],
+      ['number', 'double', [], 'number']
+    ]
+    expect(() => resolveModifierChain('10 symbols', chain, {}, [])).toThrowError(TypeError)
   })
 })
