d626a5df865f83d14bcaebbd28f3adbedde889dc
Илья Боровитинов <ilya.borovitinov@sl-tech.ru>
2019-09-09 17:29:50 +0300

    Feat: refactored model resolution into two distinct functions

--- src/components/form-generator/schema/resolution.ts
+++ src/components/form-generator/schema/resolution.ts
@@ -1,6 +1,7 @@
 import { LogicalBranch, Level, If, Elif, Switch, For, Field } from './types'
 import { isEqual } from 'lodash'
 import memoize from 'memoize-one'
+import { getByPath } from '@/utils/objectTraversal'
 
 /**
  * Resolution context for all `$each` parts of the model path
@@ -244,6 +245,39 @@ function prepareField (field: Field): PreparedBranch {
 }
 
 /**
+ * Get a path within the model with all the `$each` parts resolved based on the context
+ * 
+ * This is not a very obvious piece of code.
+ * It is here to achieve one thing: replace parts of the path based on the context
+ * even if parts of the actual path contain indexes instead of `$each` blocks.
+ *
+ * This is achieved by taking each split point from the end, and converting it
+ * to a regexp, where each `$each` part is replaced by a (\d+|\$each) regex block
+ * to account for the possible index in place of the `$each`. Moreover, this split point
+ * has to be followed by an `$each` block, which will be replaced by the index - so
+ * this part is appended to the regex as well
+ * 
+ * @param modelPath dot-separated path within the model with `$each` blocks
+ * @param context context for `$each` block resolution
+ */
+export function resolveContextPath (modelPath: string | string[], context: Context) {
+  const unpreparedPath = Array.isArray(modelPath)
+    ? modelPath.join('.')
+    : modelPath
+
+  return context
+    .reduceRight((agg, { index, splitPoint }) => {
+      const replaceable =
+        '(' +
+        splitPoint.replace(/(\$each)/g, '(\\d+|\\$each)').replace('.', '\\.') +
+        '\\.)\\$each'
+      const replacer = RegExp(replaceable)
+      return agg.replace(replacer, '$1' + String(index))
+    }, unpreparedPath)
+    .split('.')
+}
+
+/**
  * Return a value from the model based on the given dot-separated path.
  * 
  * `$each` parts are replaced with the appropriate index based on the context.
@@ -262,36 +296,7 @@ export function resolveModelPath<T = any> (
   model: any,
   context: Context
 ): T | undefined {
-  const unpreparedPath = Array.isArray(modelPath)
-    ? modelPath.join('.')
-    : modelPath
-
-  /**
-  * This is not a very obvious piece of code.
-  * It is here to achieve one thing: replace parts of the path based on the context
-  * even if parts of the actual path contain indexes instead of `$each` blocks.
-  *
-  * This is achieved by taking each split point from the end, and converting it
-  * to a regexp, where each `$each` part is replaced by a (\d+|\$each) regex block
-  * to account for the possible index in place of the `$each`. Moreover, this split point
-  * has to be followed by an `$each` block, which will be replaced by the index - so
-  * this part is appended to the regex as well
-  * 
-  */
-  const path = context
-    .reduceRight((agg, { index, splitPoint }) => {
-      const replaceable =
-        '(' +
-        splitPoint.replace(/(\$each)/g, '(\\d+|\\$each)').replace('.', '\\.') +
-        '\\.)\\$each'
-      const replacer = RegExp(replaceable)
-      return agg.replace(replacer, '$1' + String(index))
-    }, unpreparedPath)
-    .split('.')
-
-  return path.reduce((agg, pathPart, currentIndex) => {
-    return agg && agg[pathPart]
-  }, model)
+  return getByPath<T>(model, resolveContextPath(modelPath, context))
 }
 
 /**
