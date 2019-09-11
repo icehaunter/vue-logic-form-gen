259b28032b5a3647b8980b633777d461c929f73c
Илья Боровитинов <ilya.borovitinov@sl-tech.ru>
2019-09-11 18:14:30 +0300

    Tests: Added tests for memoization

--- tests/unit/resolution/preparation.spec.ts
+++ tests/unit/resolution/preparation.spec.ts
@@ -50,4 +50,190 @@ describe('prepareBranch', () => {
 
     expect(prepared._tag).toBe('level')
   })
+
+  describe('resolution memoization', () => {
+    it('should properly memoize basic model-dependant branching', () => {
+      const prepared = prepareBranch({
+        type: 'if',
+        predicate: {
+          _modelPath: 'value'
+        },
+        then: {
+          type: 'level',
+          level: 'then',
+          children: []
+        }
+      })
+
+      let resolved = prepared.resolver({ value: true, other: true }, [])
+
+      expect(resolved).toHaveProperty('_tag', 'level')
+
+      resolved = prepared.resolver({ value: true, other: false }, [])
+
+      const { cacheHit, cacheMiss } = (prepared.resolver as any).cacheStatistics
+
+      expect(cacheHit).toBe(1)
+      expect(cacheMiss).toBe(1)
+    })
+
+    it('should properly memoize basic model-dependant branching if undefined or null', () => {
+      const prepared = prepareBranch({
+        type: 'if',
+        predicate: {
+          _modelPath: 'value'
+        },
+        then: {
+          type: 'level',
+          level: 'then',
+          children: []
+        },
+        else: {
+          type: 'level',
+          level: 'else',
+          children: []
+        }
+      })
+
+      let resolved = prepared.resolver({ other: true }, [])
+
+      expect(resolved).toHaveProperty('_tag', 'level')
+
+      resolved = prepared.resolver({ other: false }, [])
+
+      resolved = prepared.resolver({ value: null, other: true }, [])
+
+      resolved = prepared.resolver({ value: null, other: false }, [])
+
+      const { cacheHit, cacheMiss } = (prepared.resolver as any).cacheStatistics
+
+      expect(cacheHit).toBe(2)
+      expect(cacheMiss).toBe(2)
+    })
+
+    // TODO: Reenable skipped test after resolution of underlying issue in the library https://github.com/theKashey/memoize-state/issues/35
+    it.skip('should properly memoize basic model-dependant branching if result is undefined', () => {
+      const prepared = prepareBranch({
+        type: 'if',
+        predicate: {
+          _modelPath: 'value'
+        },
+        then: {
+          type: 'level',
+          level: 'then',
+          children: []
+        }
+      })
+
+      let resolved = prepared.resolver({ value: false, other: true }, [])
+
+      expect(resolved).toBeUndefined()
+
+      resolved = prepared.resolver({ value: false, other: false }, [])
+
+      const { cacheHit, cacheMiss } = (prepared.resolver as any).cacheStatistics
+
+      expect(cacheHit).toBe(1)
+      expect(cacheMiss).toBe(1)
+    })
+
+    it('should properly memoize model-dependant branching with complex actions', () => {
+      const prepared = prepareBranch({
+        type: 'if',
+        predicate: {
+          _buildFrom: false,
+          _actions: [
+            ['boolean', 'or', [{ _modelPath: 'value' }], 'boolean']
+          ]
+        },
+        then: {
+          type: 'level',
+          level: 'then',
+          children: []
+        }
+      })
+
+      let resolved = prepared.resolver({ value: true, other: true }, [])
+
+      expect(resolved).toHaveProperty('_tag', 'level')
+
+      resolved = prepared.resolver({ value: true, other: false }, [])
+
+      const { cacheHit, cacheMiss } = (prepared.resolver as any).cacheStatistics
+
+      expect(cacheHit).toBe(1)
+      expect(cacheMiss).toBe(1)
+    })
+
+    it('should properly memoize model-dependant branching with actions and arrays', () => {
+      const prepared = prepareBranch({
+        type: 'if',
+        predicate: {
+          _buildFrom: {
+            _modelPath: 'value'
+          },
+          _actions: [
+            ['array', 'at', [1], 'boolean'],
+            ['boolean', 'not', [], 'boolean']
+          ]
+        },
+        then: {
+          type: 'level',
+          level: 'then',
+          children: []
+        }
+      })
+
+      let resolved = prepared.resolver({ value: [true, false], other: true }, [])
+
+      expect(resolved).toHaveProperty('_tag', 'level')
+
+      resolved = prepared.resolver({ value: [true, false, true], other: false }, [])
+
+      const { cacheHit, cacheMiss } = (prepared.resolver as any).cacheStatistics
+
+      expect(cacheHit).toBe(1)
+      expect(cacheMiss).toBe(1)
+    })
+
+    it('should properly memoize model-dependant branching with arrays and context', () => {
+      const prepared = prepareBranch({
+        type: 'if',
+        predicate: {
+          _buildFrom: {
+            _modelPath: 'value.$each'
+          },
+          _actions: [
+            ['boolean', 'not', [], 'boolean']
+          ]
+        },
+        then: {
+          type: 'level',
+          level: 'then',
+          children: []
+        }
+      })
+
+      let resolved = prepared.resolver({ value: [true, false], other: true }, [
+        {
+          splitPoint: 'value',
+          index: 1
+        }
+      ])
+
+      expect(resolved).toHaveProperty('_tag', 'level')
+
+      resolved = prepared.resolver({ value: [true, false, true], other: false }, [
+        {
+          splitPoint: 'value',
+          index: 1
+        }
+      ])
+
+      const { cacheHit, cacheMiss } = (prepared.resolver as any).cacheStatistics
+
+      expect(cacheHit).toBe(1)
+      expect(cacheMiss).toBe(1)
+    })
+  })
 })
