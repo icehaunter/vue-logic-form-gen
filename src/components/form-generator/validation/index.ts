eb080da30b7b1ef0b9f30495a5a9310fec356838
Ilya Borovitinov <icehaunter@gmail.com>
2019-09-20 14:25:30 +0300

    Feat: added validation collection from the resolved schema

--- src/components/form-generator/validation/index.ts
+++ src/components/form-generator/validation/index.ts
@@ -1,6 +1,7 @@
 import { ValidatorsSchema, validators } from './validators'
 import { resolveValue } from '../logic'
 import { Context } from '../resolution'
+import { ResolutionResult } from '../resolution/resolution'
 
 export interface PreparedValidator extends Omit<ValidatorsSchema, 'params'> {
   predicate: (value: any) => boolean
@@ -25,5 +26,21 @@ export const prepareValidator = (model: any, context: Context) => (validator: Va
     errorMessage: validator.errorMessage,
     level: validator.level,
     predicate: predicate
+  }
+}
+
+type ModelValidators = [string, PreparedValidator[]]
+
+export function collectValidators (resolved: ResolutionResult): Array<ModelValidators> {
+  if (resolved === undefined) {
+    return []
+  } else if (Array.isArray(resolved)) {
+    return resolved.flatMap(collectValidators)
+  } else if (resolved.type === 'level') {
+    return resolved.children.flatMap(collectValidators)
+  } else if (resolved.validation !== undefined) {
+    return [[resolved.modelPath, resolved.validation]]
+  } else {
+    return []
   }
 }
--- tests/unit/validation/index.spec.ts
+++ tests/unit/validation/index.spec.ts
@@ -1,4 +1,5 @@
-import { prepareValidator } from '@/components/form-generator/validation'
+import { prepareValidator, collectValidators, PreparedValidator } from '@/components/form-generator/validation'
+import { prepareBranch, resolveTree } from '@/components/form-generator/resolution'
 
 describe('validator preparation', () => {
   it('should properly prepare a simple validator', () => {
@@ -92,5 +93,208 @@ describe('validator preparation', () => {
     expect(result.predicate('1')).toBe(true)
     expect(result.predicate('12')).toBe(true)
     expect(result.predicate('123')).toBe(false)
+  })
+})
+
+describe('validator collection', () => {
+  it('should properly collect a validator from a prepared field', () => {
+    const prepared = prepareBranch({
+      type: 'field',
+      modelPath: 'test',
+      widget: {
+        type: 'span'
+      },
+      validation: [
+        {
+          type: 'always',
+          level: 'error',
+          errorMessage: 'always error'
+        }
+      ]
+    })
+
+    const resolved = resolveTree(prepared, {})
+
+    const validations = collectValidators(resolved)
+
+    expect(validations).toHaveLength(1)
+    expect(validations[0]).toHaveLength(2)
+    expect(validations[0][0]).toBe('test')
+    expect(validations[0][1]).toHaveLength(1)
+    expect(validations[0][1][0]).toMatchObject({
+      errorMessage: 'always error',
+      level: 'error',
+      type: 'always'
+    })
+    expect(validations[0][1][0]).toHaveProperty('predicate')
+    expect(validations[0][1][0].predicate(true)).toBe(true)
+    expect(validations[0][1][0].predicate(false)).toBe(true)
+  })
+
+  it('should properly collect a validator from a prepared level', () => {
+    const prepared = prepareBranch({
+      type: 'level',
+      level: 'top',
+      children: [
+        {
+          type: 'field',
+          modelPath: 'test',
+          widget: {
+            type: 'span'
+          },
+          validation: [
+            {
+              type: 'always',
+              level: 'error',
+              errorMessage: 'always error'
+            }
+          ]
+        }
+      ]
+    })
+
+    const resolved = resolveTree(prepared, {})
+
+    const validations = collectValidators(resolved)
+
+    expect(validations).toHaveLength(1)
+    expect(validations[0]).toHaveLength(2)
+    expect(validations[0][0]).toBe('test')
+    expect(validations[0][1]).toHaveLength(1)
+    expect(validations[0][1][0]).toMatchObject({
+      errorMessage: 'always error',
+      level: 'error',
+      type: 'always'
+    })
+    expect(validations[0][1][0]).toHaveProperty('predicate')
+    expect(validations[0][1][0].predicate(true)).toBe(true)
+    expect(validations[0][1][0].predicate(false)).toBe(true)
+  })
+
+  it('should return an empty array if no validations were specified', () => {
+    const prepared = prepareBranch({
+      type: 'level',
+      level: 'top',
+      children: [
+        {
+          type: 'field',
+          modelPath: 'test',
+          widget: {
+            type: 'span'
+          }
+        }
+      ]
+    })
+
+    const resolved = resolveTree(prepared, {})
+
+    const validations = collectValidators(resolved)
+
+    expect(validations).toEqual([])
+  })
+
+  it('should return all validators if for-loop is present', () => {
+    const prepared = prepareBranch({
+      type: 'for',
+      modelPath: 'arr',
+      schema: {
+        type: 'field',
+        modelPath: 'arr.$each',
+        widget: {
+          type: 'span'
+        },
+        validation: [
+          { type: 'always', level: 'error', errorMessage: 'error' }
+        ]
+      }
+    })
+
+    const resolved = resolveTree(prepared, {
+      arr: ['a', 'b']
+    })
+
+    const validations = collectValidators(resolved)
+
+    expect(validations).toHaveLength(2)
+    expect(validations.map(([path]) => path)).toEqual(['arr.0', 'arr.1'])
+  })
+
+  it('should return empty array if tree was resolved to `undefined`', () => {
+    const prepared = prepareBranch({
+      type: 'if',
+      predicate: false,
+      then: {
+        type: 'field',
+        modelPath: 'test',
+        widget: {
+          type: 'span'
+        },
+        validation: [
+          {
+            type: 'always',
+            errorMessage: 'error',
+            level: 'error'
+          }
+        ]
+      }
+    })
+
+    const resolved = resolveTree(prepared, {})
+
+    const validations = collectValidators(resolved)
+
+    expect(validations).toEqual([])
+  })
+
+  it('should properly collect only validators in resolved branches', () => {
+    const prepared = prepareBranch({
+      type: 'if',
+      predicate: false,
+      then: {
+        type: 'field',
+        modelPath: 'test',
+        widget: {
+          type: 'span'
+        },
+        validation: [
+          {
+            type: 'always',
+            errorMessage: 'error',
+            level: 'error'
+          }
+        ]
+      },
+      else: {
+        type: 'field',
+        modelPath: 'other',
+        widget: {
+          type: 'span'
+        },
+        validation: [
+          {
+            type: 'always',
+            errorMessage: 'error',
+            level: 'error'
+          }
+        ]
+      }
+    })
+
+    const resolved = resolveTree(prepared, {})
+
+    const validations = collectValidators(resolved)
+
+    expect(validations).toHaveLength(1)
+    expect(validations[0]).toHaveLength(2)
+    expect(validations[0][0]).toBe('other')
+    expect(validations[0][1]).toHaveLength(1)
+    expect(validations[0][1][0]).toMatchObject({
+      errorMessage: 'error',
+      level: 'error',
+      type: 'always'
+    })
+    expect(validations[0][1][0]).toHaveProperty('predicate')
+    expect(validations[0][1][0].predicate(true)).toBe(true)
+    expect(validations[0][1][0].predicate(false)).toBe(true)
   })
 })
