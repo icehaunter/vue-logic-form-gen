f709cd9cdbd1d321084c262812f377c2e838c2cd
Илья Боровитинов <ilya.borovitinov@sl-tech.ru>
2019-09-25 18:57:21 +0300

    Added typescript to publishing

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  parallel: false,
+  chainWebpack: config => {
+    config.module
+      .rule('ts')
+      .use('ts-loader')
+      .loader('ts-loader')
+      .tap(opts => {
+        opts.transpileOnly = false
+        opts.happyPackMode = false
+        return opts
+      })
+  }
+}
