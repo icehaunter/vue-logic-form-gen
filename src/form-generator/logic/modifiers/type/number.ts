ec31249fa7ece28a060835dd4f38c44e8efb608e
Илья Боровитинов <ilya.borovitinov@sl-tech.ru>
2019-09-27 09:56:22 +0300

    Feat: added number comparison operators

--- src/form-generator/logic/modifiers/type/number.ts
+++ src/form-generator/logic/modifiers/type/number.ts
@@ -5,6 +5,11 @@ export type NumberModifierTypes = {
   div: (left: number, right: number) => number
   double: (target: number) => number
   toString: (target: number) => string
+  eq: (left: number, right: number) => boolean
+  lt: (left: number, right: number) => boolean
+  lte: (left: number, right: number) => boolean
+  gt: (left: number, right: number) => boolean
+  gte: (left: number, right: number) => boolean
 }
 
 export const modifiers: NumberModifierTypes = {
@@ -13,5 +18,10 @@ export const modifiers: NumberModifierTypes = {
   mul: (left, right) => left * right,
   div: (left, right) => left / right,
   double: target => target * target,
-  toString: target => target.toString()
+  toString: target => target.toString(),
+  eq: (l, r) => l === r,
+  lt: (l, r) => l < r,
+  lte: (l, r) => l <= r,
+  gt: (l, r) => l > r,
+  gte: (l, r) => l >= r
 }
